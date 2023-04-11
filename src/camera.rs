9f9a072e40bfcf68849f7ba5333936956510a891
jonathan <jonathan.rinna@gmail.com>
2023-04-11 09:03:25 +0200

    camera class incoming

--- src/camera.rs
+++ src/camera.rs
@@ -5,7 +5,6 @@ type Point3 = Vec3;
 pub struct Camera {
 	ray: [Vec3],
 	origin: Point3,
-	aspect_ratio: f64,
 	heigth: u16,
 	width: u16,
 	fov: f64,
@@ -16,8 +15,15 @@ pub struct Camera {
 
 impl Camera {
 
-	pub fn new (heigth: u16, aspect_ratio: f64, fov: f64, origin: Point3) -> Self {
-		let mut camera = Camera { ray: [], origin, aspect_ratio, heigth, width: (aspect_ratio * heigth as f64).into(), fov, depth_unit_vector: Vec3::new(1, 0, 0) };
-		
+	pub fn new (heigth: u16, width: u16, fov: f64, origin: Point3) -> Self {
+		let mut camera = Camera { ray: [], origin, heigth, width, fov,
+			depth_unit_vector: Vec3::new(1, 0, 0), width_unit_vector: Vec3::new(0, 0, 1),
+			heigth_unit_vector: Vec3::new(0, 1, 0) };
+		let depth: f64 = (width / 2.0) / (fov / 2.0).tan();
+		for h in 0..heigth {
+			for w in 0..width {
+				let 
+			}
+		}
 	}
 }
