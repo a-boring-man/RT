11d6922227137abe0527ca0684430c9f1709f67b
jonathan <jonathan.rinna@gmail.com>
2023-05-10 08:38:29 +0200

    waiting bus

--- src/keynput.rs
+++ src/keynput.rs
@@ -5,11 +5,11 @@ use crate::camera::Camera;
 pub fn handle_keypress(input: KeyboardInput, mut camera: &Camera) {
 	println!("key pressed : {}", input.scancode);
 	match input.scancode {
-		17 => {move_front(5);}
+		17 => {move_front(5, camera);}
 		_ => {}
 	}
 }
 
-fn move_front(distance: i16, camera) {
-
+fn move_front(distance: i16, mut camera: &Camera) {
+	
 }
