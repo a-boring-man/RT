522560457fe9413b546b4ab3bf59c3fd0c7e29b5
jonathan <jonathan.rinna@gmail.com>
2023-07-03 09:15:07 +0200

    fixing translation

--- src/keynput.rs
+++ src/keynput.rs
@@ -2,25 +2,26 @@ use winit::event::KeyboardInput;
 
 use crate::camera::Camera;
 use crate::matrix::Matrix;
+use crate::vec3::Vec3;
 
 pub fn handle_keypress(input: KeyboardInput, camera: &mut Camera) {
 	//println!("key pressed : {}", input.scancode);
 	let mut work_to_be_done = true;
 	let mut translation = false;
 	let mut rotation_matrix = Matrix::<f64>::new_identity(3, 3);
-	let mut translation_matrix =
+	let mut translation_vector = Vec3::new(0.0, 0.0, 0.0);
 	match input.scancode {
-		17 => {rotation_matrix = camera.translation_x.clone();
+		17 => {translation_vector = camera.translation_x.clone();
 				translation = true;}
-		31 => {rotation_matrix = camera.translation_x_neg.clone();
+		31 => {translation_vector = camera.translation_x_neg.clone();
 			translation = true;}
-		32 => {rotation_matrix = camera.translation_z.clone();
+		32 => {translation_vector = camera.translation_z.clone();
 			translation = true;}
-		30 => {rotation_matrix = camera.translation_z_neg.clone();
+		30 => {translation_vector = camera.translation_z_neg.clone();
 			translation = true;}
-		57 => {rotation_matrix = camera.translation_y.clone();
+		57 => {translation_vector = camera.translation_y.clone();
 			translation = true;}
-		42 => {rotation_matrix = camera.translation_y_neg.clone();
+		42 => {translation_vector = camera.translation_y_neg.clone();
 			translation = true;}
 		57416 => {rotation_matrix = camera.rot_z.clone();}
 		57424 => {rotation_matrix = camera.rot_z_neg.clone();}
@@ -35,7 +36,7 @@ pub fn handle_keypress(input: KeyboardInput, camera: &mut Camera) {
 	}
 	if translation {
 		println!("work : {:?}", rotation_matrix.clone());
-		camera.set_origin(rotation_matrix.clone() * camera.get_origin());
+		camera.set_origin(translation_vector.clone() + camera.get_origin());
 	}
 	camera.set_direction(rotation_matrix.clone() * camera.get_direction());
 	camera.set_width(rotation_matrix.clone() * camera.get_width());
